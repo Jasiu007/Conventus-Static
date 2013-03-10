32eaf3be8e32cf51fc2201f3c1c23d9ea8b0fcd7
Jasiu007 <jsebastian022@gmail.com>
2013-03-10 13:36:57 +0000

    Front-End Update. Added Profile Page.

--- js/base.js
+++ js/base.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    // Drop Down Options *************************************************************
+    
 	function DropDown(el)
 		{
 			this.profession = el;
@@ -51,6 +53,51 @@ $(document).ready(function(){
 			$('.wrapper').removeClass('active');
 
 		});
+
+	// Tabs Menu System **************************************************************
+
+	function resetTabs(){
+
+    $("#content > div").hide(); //Hide all content
+    $("#tabs a").attr("id",""); //Reset id's
+
+	}
+
+	var myUrl = window.location.href; //get URL
+	var myUrlTab = myUrl.substring(myUrl.indexOf("#")); // For mywebsite.com/tabs.html#tab2, myUrlTab = #tab2     
+	var myUrlTabName = myUrlTab.substring(0,4); // For the above example, myUrlTabName = #tab
+
+
+
+    $("#content > div").hide(); // Initially hide all content
+    $("#tabs li:first a").attr("id","current"); // Activate first tab
+    $("#content > div:first").fadeIn(); // Show first tab content
+    
+    $("#tabs a").on("click",function(e) {
+
+        e.preventDefault();
+
+        if ($(this).attr("id") == "current")
+        { //detection for current tab
+        	 return       
+        }
+        else
+        {             
+        	resetTabs();
+        	$(this).attr("id","current"); // Activate this
+        	$($(this).attr('name')).fadeIn(); // Show content for current tab
+        }
+    });
+
+    for (i = 1; i <= $("#tabs li").length; i++) {
+     	
+     	if (myUrlTab == myUrlTabName + i) 
+     	{
+          resetTabs();
+          $("a[name='"+myUrlTab+"']").attr("id","current"); // Activate url tab
+          $(myUrlTab).fadeIn(); // Show url tab content        
+      	}
+    }
 
 
 });
